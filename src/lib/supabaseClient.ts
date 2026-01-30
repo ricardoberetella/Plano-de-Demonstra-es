3ddecaefd0f59189cf9d66e4490d0f4a7fc3ab3e
ricardoberetella <ricardoberetella20@gmail.com>
2026-01-30 16:35:32 -0300

    Delete src/lib directory

--- src/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://iwwmfdnehzecxpxnnftc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3d21mZG5laHplY3hweG5uZnRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3OTU5NzksImV4cCI6MjA4NTM3MTk3OX0.qRraMNP5v-Cqf6hHmTRQMQQfmsTziBG3vi7nRWyBYew';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
